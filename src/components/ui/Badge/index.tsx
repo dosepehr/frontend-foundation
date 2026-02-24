242226f8185878baf2a21c58910986371fc0ed98
dosepehr <dosepehr@gmail.com>
2026-02-24 22:12:36 +0330

    add badege component

---
+++ src/components/ui/Badge/index.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { Slot } from 'radix-ui';
+import { cn } from '@/utils/funcs/cn';
+
+const badgeVariants = cva(
+    'h-5 gap-1 rounded-4xl border border-transparent px-2 py-0.5 text-xs font-medium transition-all has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&>svg]:size-3! inline-flex items-center justify-center w-fit whitespace-nowrap shrink-0 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive overflow-hidden group/badge',
+    {
+        variants: {
+            variant: {
+                default:
+                    'bg-primary text-primary-foreground [a]:hover:bg-primary/80',
+                secondary:
+                    'bg-secondary text-secondary-foreground [a]:hover:bg-secondary/80',
+                destructive:
+                    'bg-destructive/10 [a]:hover:bg-destructive/20 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 text-destructive dark:bg-destructive/20',
+                outline:
+                    'border-border text-foreground [a]:hover:bg-muted [a]:hover:text-muted-foreground',
+                ghost: 'hover:bg-muted hover:text-muted-foreground dark:hover:bg-muted/50',
+                link: 'text-primary underline-offset-4 hover:underline',
+                info: 'bg-blue-500/10 text-blue-600 dark:bg-blue-500/20 dark:text-blue-400 [a]:hover:bg-blue-500/20 dark:[a]:hover:bg-blue-500/30 focus-visible:ring-blue-500/20 dark:focus-visible:ring-blue-500/40',
+                success:
+                    'bg-green-500/10 text-green-600 dark:bg-green-500/20 dark:text-green-400 [a]:hover:bg-green-500/20 dark:[a]:hover:bg-green-500/30 focus-visible:ring-green-500/20 dark:focus-visible:ring-green-500/40',
+                warning:
+                    'bg-amber-500/10 text-amber-600 dark:bg-amber-500/20 dark:text-amber-400 [a]:hover:bg-amber-500/20 dark:[a]:hover:bg-amber-500/30 focus-visible:ring-amber-500/20 dark:focus-visible:ring-amber-500/40',
+            },
+        },
+        defaultVariants: {
+            variant: 'default',
+        },
+    }
+);
+
+function Badge({
+    className,
+    variant = 'default',
+    asChild = false,
+    ...props
+}: React.ComponentProps<'span'> &
+    VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
+    const Comp = asChild ? Slot.Root : 'span';
+
+    return (
+        <Comp
+            data-slot='badge'
+            data-variant={variant}
+            className={cn(badgeVariants({ variant }), className)}
+            {...props}
+        />
+    );
+}
+
+export { Badge, badgeVariants };
+
