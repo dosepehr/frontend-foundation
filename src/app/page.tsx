39709a0dcf97d94c15ba51882ad34afde9154dc3
dosepehr <dosepehr@gmail.com>
2026-02-26 02:21:18 +0330

    add some components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
-import ThemeChange from '@/components/common/ThemeChange';
+'use client';
 import { Checkbox } from '@/components/ui/Checkbox';
+import { ComboboxPopup } from '@/components/ui/ComboBox';
 import { Label } from '@/components/ui/Label';
 const page = () => {
     return (
         <>
-            <ThemeChange />
             <div className='flex gap-2'>
                 <Checkbox id='terms' checked='indeterminate' />
                 <Label htmlFor='terms'>Accept terms and conditions</Label>
             </div>
+            <ComboboxPopup />
         </>
     );
 };
