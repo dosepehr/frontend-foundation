59809581c405af212ff05c08a309112404d7d077
dosepehr <dosepehr@gmail.com>
2026-06-14 22:06:45 +0330

    feat(stories): add react scan addon

---
+++ .storybook/addons/react-scan/constants.ts
@@ -0,0 +1,2 @@
+export const REACT_SCAN_GLOBAL_TYPE_ID = "reactScan";
+export const DEFAULT_REACT_SCAN = "false";
---
+++ .storybook/addons/react-scan/decorator.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import type { ComponentType } from "react";
+import type { Decorator } from "@storybook/react";
+import { useEffect } from "react";
+import { scan } from "react-scan";
+import { useGlobals } from "storybook/preview-api";
+import { REACT_SCAN_GLOBAL_TYPE_ID } from "./constants";
+
+export const withReactScan: Decorator = (Story) => {
+  const [globals] = useGlobals();
+  const isEnabled = globals[REACT_SCAN_GLOBAL_TYPE_ID] === "true";
+
+  useEffect(() => {
+    scan({ enabled: isEnabled });
+  }, [isEnabled]);
+
+  const S = Story as ComponentType;
+  return <S />;
+};
