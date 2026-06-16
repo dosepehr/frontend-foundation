9560b26df67e9fa19d4073cdf7f711101e14ff99
dosepehr <dosepehr@gmail.com>
2026-06-16 23:19:10 +0330

    feat(components): add otp input component

---
+++ src/components/controlled/ControlledOtpInput/index.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { Controller, type Control, type FieldValues, type Path } from 'react-hook-form'
+import { OtpInput } from '../../ui/OtpInput'
+import type { OtpInputProps } from '../../ui/OtpInput'
+
+export type ControlledOtpInputProps<T extends FieldValues> = Omit<
+    OtpInputProps,
+    'value' | 'onChange'
+> & {
+    name: Path<T>
+    control: Control<T>
+    onValueChange?: (value: string) => void
+}
+
+export const ControlledOtpInput = <T extends FieldValues>({
+    name,
+    control,
+    onValueChange,
+    error,
+    ...props
+}: ControlledOtpInputProps<T>) => (
+    <Controller
+        name={name}
+        control={control}
+        render={({ field, fieldState }) => (
+            <OtpInput
+                {...props}
+                value={field.value ?? ''}
+                onChange={(val) => {
+                    field.onChange(val)
+                    onValueChange?.(val)
+                }}
+                error={error ?? fieldState.error?.message}
+            />
+        )}
+    />
+)
